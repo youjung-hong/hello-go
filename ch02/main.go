95e550844e2ac05bc602e5ca72ea869a472c7f4b
youjung-hong <dev.youjung.hong@gmail.com>
2024-11-25 21:22:28 +0900

    ch02. 명시적인 형변환 필수

--- ch02/main.go
+++ ch02/main.go
@@ -12,15 +12,11 @@ func main() {
 	a := 3
 	var b float64 = 3.5
 
-	var c int = b // int에 float64 할당할 수 없다
-	d := a * b    // int * float64 할 수 없다
+	var c int = int(b)  // float64를 int로 형변환
+	d := float64(a * c) // int * int를 float64로 형변환
 
 	var e int64 = 7
-	f := a * e // int * int64 할 수 없다
+	f := int64(a) * e // int64 * int64
 
 	fmt.Print(a, b, c, d, e, f)
-
-	// ./main.go:15:14: cannot use b (variable of type float64) as int value in variable declaration
-	// ./main.go:16:7: invalid operation: a * b (mismatched types int and float64)
-	// ./main.go:19:7: invalid operation: a * e (mismatched types int and int64) //
 }
