b68ae21fdd707687eefa71678b64e92c5c086e93
youjung-hong <dev.youjung.hong@gmail.com>
2024-11-25 21:35:46 +0900

    ch02. 더 작은 크기의 변수로 변환되면, 오버플로우된 값들이 사라짐

--- ch02/main.go
+++ ch02/main.go
@@ -9,19 +9,25 @@ func main() {
 	// d := 5
 
 	// fmt.Print(a, b, c, d)
-	a := 3
-	var b float64 = 3.5
+	// a := 3
+	// var b float64 = 3.5
 
-	var c int = int(b)  // float64를 int로 형변환
-	d := float64(a * c) // int * int를 float64로 형변환
+	// var c int = int(b)  // float64를 int로 형변환
+	// d := float64(a * c) // int * int를 float64로 형변환
 
-	var e int64 = 7
-	f := int64(a) * e // int64 * int64
+	// var e int64 = 7
+	// f := int64(a) * e // int64 * int64
 
-	fmt.Print(a, b, c, d, e, f)
+	// fmt.Print(a, b, c, d, e, f)
 
-	var g int = int(b * 3)
-	var h int = int(b) * 3
+	// var g int = int(b * 3)
+	// var h int = int(b) * 3
 
-	fmt.Print(g, h)
+	// fmt.Print(g, h)
+	var a int16 = 3456
+	var b int8 = int8(a)
+	var c int8 = 127
+	var d int8 = -127
+
+	fmt.Print(a, b, c, d)
 }
