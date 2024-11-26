688039f09d21122d801b0dd4589fdb48ab5bc018
youjung-hong <dev.youjung.hong@gmail.com>
2024-11-26 10:43:37 +0900

    ch03. 에러났을 때 표준입력값 리셋

--- ch03/main.go
+++ ch03/main.go
@@ -1,15 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
+	stdin := bufio.NewReader(os.Stdin)
+
 	var a int
 	var b int
 
-	n, err := fmt.Scan(&a, &b)
+	n, err := fmt.Scanf("%d %d", &a, &b)
 	if err != nil { // 에러가 발생하면
 		fmt.Println(n, err)
+		stdin.ReadString('\n')
 	} else { // 정상적으로 입력받으면
 		fmt.Println(n, a, b)
 	}
+
+	fmt.Print(":)")
 }
